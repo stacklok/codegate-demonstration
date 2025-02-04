85eefc059186bc4da0f220c2adbe38fe7f1517b0
Luke Hinds <luke@stacklok.com>
2025-02-04 09:50:34 +0000

    Add Some PII examples

--- pii/piitest.py
+++ pii/piitest.py
@@ -1,13 +1,13 @@
 import pytest
 
 def test_passing():
-    email = "lukehinds@gmail.com"
-    assert email == "luke@stacklok.com"
+    email = "jeff@example.com"
+    assert email == "jimnull@example.com"
     with pytest.raises:
         raise ("This is a configuration error")
 
 def test_failing():
-    email = "johnsmith@hotmail.com"
-    assert email == "jeffbrown@bbc.com"
+    email = "janet@example.com"
+    assert email == "janetnull@example.com"
     with pytest.raises():
         raise ("This is a configuration error")
